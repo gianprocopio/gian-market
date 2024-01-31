c95461b5eea06f0fe78fbff7d3c8ac1cb5ea54f5
Gianluca Procopio <gianplay024@gmail.com>
2024-01-31 12:36:03 -0300

    Project structure: files and folders required for an API

---
+++ src/main/java/com/gian/market/GianMarketApplication.java
@@ -0,0 +1,13 @@
+package com.gian.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GianMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GianMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gian/market/GianMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gian.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GianMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
